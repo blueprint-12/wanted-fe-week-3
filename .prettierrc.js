4fa6db7452bbc9f3e108a0ecc291da39f7e32bdc
blueprint-12 <blueprint12@daum.net>
2023-07-11 21:48:19 +0900

    chore: INIT

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    tabWidth: 2, // 탭 너비
+    bracketSameLine: true, // HTML 태그시 닫는 모양 > 같은 라인으로
+    arrowParens: "avoid", // x => x 가능
+    trailingComma: "none", // 객체 마지막 요소 , 없애기
+    trailingComma: "none" // 객체 마지막 요소 , 없애기,
+  };
