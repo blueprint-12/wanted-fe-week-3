c8cd2afcfc541c05b3be0e30f4126c04714cb22b
blueprint-12 <blueprint12@daum.net>
2023-07-12 23:59:16 +0900

    feat: gh API test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import api from "./utils/api/customAxios";
 
 function App() {
-  return <div className="App">안녕</div>;
+  const [content, setContent] = useState([]);
+  useEffect(() => {
+    something();
+  }, []);
+
+  const something = async () =>
+    await api
+      .get("/facebook/react/issues")
+      .then(res => {
+        console.log("res.data", res.data);
+        setContent(res.data);
+      })
+      .catch(err => console.log("err from API", err.data));
+
+  return (
+    <div className="App">
+      {content ? content.length : <span>loading...</span>}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/utils/api/customAxios.ts
@@ -0,0 +1,63 @@
+import axios from "axios";
+
+console.log(process.env.REACT_APP_GH_TOKEN);
+const token = process.env.REACT_APP_GH_TOKEN;
+
+interface CustomError {
+  status: number;
+  message: string;
+}
+
+const BASE_URL = "https://api.github.com/repos/";
+
+export const api = axios.create({
+  baseURL: BASE_URL,
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+    "X-GitHub-Api-Version": "2022-11-28"
+  }
+});
+
+//요청 API
+api.interceptors.request.use(
+  config => {
+    // let token = getToken();
+    // if (getToken() && config.headers) {
+    //   config.headers.Authorization = `Bearer ${token}`;
+    // }
+    return config;
+  },
+  err => {
+    if (err.response) {
+      const { status, data } = err.response;
+      const errResponse: CustomError = {
+        status,
+        message: data.message
+      };
+      return Promise.reject(errResponse);
+    }
+    return Promise.reject(err);
+  }
+);
+
+//응답 API
+api.interceptors.response.use(
+  response => {
+    return response;
+  },
+  err => {
+    if (err.response) {
+      const { status, data } = err.response;
+      const errResponse: CustomError = {
+        status,
+        message: data.message
+      };
+      return Promise.reject(errResponse);
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default api;
